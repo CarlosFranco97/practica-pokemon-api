2ee1d45416cb492223b1d23310358efbed3d4865
carlosfranco97 <@carlosfranco63.cf@gmail.com>
2023-08-11 12:50:33 -0400

    7mo commit: Se le agrega al InputPokemon la funcion handleSubmit y handleInputChange

--- src/components/InputPokemon.jsx
+++ src/components/InputPokemon.jsx
@@ -1,10 +1,35 @@
+import { useReducer } from "react"
 import { BotonBuscar } from "./BotonBuscar"
 export const InputPokemon = () => {
-  return (
-  <form>
+  //Para controlar el valor del input: 
+    const [inputValue, dispatch] = useReducer((state = [], action) => {
+    switch(action.type) {
+        case 'Change_Input': 
+        { const newValueInput = action.payload
+        return newValueInput
+        }
+    default: 
+    return state
+    }
+    });
+
+    const handleInputChnage = ({target}) => {
+        dispatch({
+            type: 'Change_Input',
+            payload: target.value
+        })
+    }
+  
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }  
+return (
+  <form onSubmit={handleSubmit}>
     <input type="text" 
     placeholder="Buscar Pokemon"
-    />
+    value={inputValue}
+    onChange={handleInputChnage}
+    /> 
     <BotonBuscar />
   </form>  
  )
