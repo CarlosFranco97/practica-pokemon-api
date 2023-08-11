1710a9cc35d6c1dba83dcfe137699cd967113e01
carlosfranco97 <@carlosfranco63.cf@gmail.com>
2023-08-11 12:36:58 -0400

    6to commit: Se agrega el componente BotonBuscar y se coloca en el InputPokemon

---
+++ src/components/BotonBuscar.jsx
@@ -0,0 +1,6 @@
+
+export const BotonBuscar = () => {
+  return (
+    <button>Buscar</button>
+  )
+}
--- src/components/InputPokemon.jsx
+++ src/components/InputPokemon.jsx
@@ -1,10 +1,11 @@
-
+import { BotonBuscar } from "./BotonBuscar"
 export const InputPokemon = () => {
   return (
   <form>
     <input type="text" 
     placeholder="Buscar Pokemon"
     />
+    <BotonBuscar />
   </form>  
  )
 }
